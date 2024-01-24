5fc89f6b5a4308eef4dfe054db1fb6297bd22624
VictorVRS <victorrodrigosilva@gmail.com>
2024-01-24 13:20:17 -0300

    Set up project initial organization and metadata closes #8

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward point System fo Dunder Miflin",
+    author="Victor Rodrigo",
+    packages=find_packages(),
+)
